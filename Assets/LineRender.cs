e25399cadf95ddb52ba895f3a2a6f3c22ea35f5e
Trasqual <59961702+Trasqual@users.noreply.github.com>
2021-02-01 17:45:54 +0300

    Initial commit

---
+++ Assets/LineRender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRender : MonoBehaviour
+{
+    LineRenderer lr;
+
+    public List<Transform> lamps = new List<Transform>();
+    Vector3[] positions = new Vector3[7];
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < lamps.Count; i++)
+        {
+            positions[i] = lamps[i].position;
+        }
+        lr.SetPositions(positions);
+    }
+}
